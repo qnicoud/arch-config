7c94022ea192ec711c75029ab31d05ef1056883b
Quentin Nicoud <codeit@kantan.aleeas.com>
2025-04-22 21:24:09 +0200

    Update symlink command in case needed

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ SOURCE=$(dirname $(readlink -f ${BASH_SOURCE}))
 for dir in $(find $SOURCE -mindepth 1 -maxdepth 1 -type d -not -name "*.git" ) ; do
     rm ${HOME}/.config/$(basename $dir)
     cp -r $dir "${HOME}/.config/"
-    #ln -s $dir ~/.config/
+    #ln -s $dir "${HOME}/.config/"
 done
