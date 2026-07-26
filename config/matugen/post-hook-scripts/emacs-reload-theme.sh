e4c6cb511b2641705c2a495810980e481c067ee3
Quentin Nicoud <codeit@kantan.aleeas.com>
2026-07-26 22:56:22 +0200

    update matugen config for discord emacs gtk

---
+++ config/matugen/post-hook-scripts/emacs-reload-theme.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+emacsclient -e "(load-theme 'doom-matugen nil t)"
--- config/matugen/post-hook-scripts/gtk-themes-reload.sh
+++ config/matugen/post-hook-scripts/gtk-themes-reload.sh
@@ -4,8 +4,10 @@ current=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [[ "$current" == "'prefer-dark'" ]]; then
     gsettings set org.gnome.desktop.interface color-scheme prefer-light
+    sleep 1
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 else
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+    sleep 1
     gsettings set org.gnome.desktop.interface color-scheme prefer-light
 fi
